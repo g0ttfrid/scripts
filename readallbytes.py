10685a49ec86f8b604cc567e7fe98af2751ca8a5
Yuri M <yurimaia@protonmail.ch>
2022-08-03 22:22:38 -0300

    Update readallbytes.py

--- readallbytes.py
+++ readallbytes.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python3
 import base64
-import argparse
+from argparse import ArgumentParser
+from argparse import RawTextHelpFormatter
+
 
 # https://github.com/FortyNorthSecurity/RandomScripts/blob/main/Cobalt%20Scripts/shellcode_formatter.py
 
 def parse_args():
-    parser = argparse.ArgumentParser(usage='python3 readallbytes.py  -f shellcode.bin')
+    parser = ArgumentParser(usage='python3 readallbytes.py  -f shellcode.bin', formatter_class=RawTextHelpFormatter)
     parser.add_argument('-b', '--bin', required=True, help="Binary file")
-    parser.add_argument('-f', '--format', type=str, required=True, help="blob_b64 / shellcode / cs_shellcode / cs_b64")
+    parser.add_argument('-f', '--format', type=str, required=True, help="blob_b64 = Binary Blob base64 encoded\n"
+    "shellcode = Standard shellcode format\n"
+    "csharp = C# formatted shellcode\n"
+    "csharp_b64 = Base64 encoded C# shellcode")
     return parser.parse_args()
 
 def formatter(bin, format):
     try:
         with open(bin, "rb") as f:
             data = f.read()
-    except IOError:
+    except:
         print('[!] Error while opening the file!')  
 
-    encoded_raw = base64.b64encode(data)
-    binary_code = ''
-    for byte in data:
-        binary_code += "\\x" + hex(byte)[2:].zfill(2)
-    cs = "0" + ",0".join(binary_code.split("\\")[1:])
-    cs_shellcode = f"static byte[] buf = new byte[{cs.count('x')}] {{ {cs} }};"
-    encoded_cs = base64.b64encode(cs_shellcode.encode())
+    try:
+        encoded_raw = base64.b64encode(data)
+        binary_code = ''
+        for byte in data:
+            binary_code += "\\x" + hex(byte)[2:].zfill(2)
+        cs = "0" + ",0".join(binary_code.split("\\")[1:])
+        csharp = f"static byte[] buf = new byte[{cs.count('x')}] {{ {cs} }};"
+        encoded_cs = base64.b64encode(csharp.encode())
+    except:
+        print('[!] Error formatting the file!')
     
     if format == "blob_b64":
         print("[+] Binary Blob base64 encoded:\n")
@@ -31,10 +39,10 @@ def formatter(bin, format):
     elif format == "shellcode":
         print("[+] Standard shellcode format:\n")
         print(binary_code)
-    elif format == "cs_shellcode":
+    elif format == "csharp":
         print("[+] C# formatted shellcode:\n")
-        print(cs_shellcode)
-    elif format == "cs_b64":
+        print(csharp)
+    elif format == "csharp_b64":
         print("[+] Base64 encoded C# shellcode:\n")
         print(encoded_cs.decode('ascii'))
     else:
