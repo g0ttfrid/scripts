bb0192a1fdc2eceb7622934219088fe269103574
g0ttfrid <yurimaia@protonmail.ch>
2023-04-28 21:35:33 -0300

    Update readallbytes.py

--- readallbytes.py
+++ readallbytes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import base64
+import hashlib
+from Crypto.Cipher import AES
 from os import urandom
 from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter
@@ -18,38 +20,56 @@ def parse_args():
 	"base64 = Binary blob base64 encoded\n"
 	"csharp = C# formatted shellcode\n"
 	"nim = Nim formatted shellcode\n"
-	"xor = Xor encrypt shellcode")
+	"xor = Xor encrypt shellcode\n"
+	"aes = AES encrypt shellcode")
 	return parser.parse_args()
 
-def xor(data):
+def pad(s):
+	return s + bytes((AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size), encoding='utf-8')
+
+def aesenc(plaintext):
+	key = urandom(16)
+	k = hashlib.sha256(key).digest()
+	iv = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+	plaintext = pad(plaintext)
+	cipher = AES.new(k, AES.MODE_CBC, iv)
+	ciphertext = cipher.encrypt(bytes(plaintext))
+
+	print('AESkey[] = { 0x' + ',0x'.join(hex(x)[2:] for x in key) + ' };')
+	output = 'payload[] = { 0x' + ',0x'.join(hex(x)[2:] for x in ciphertext) + ' };'
 	
+	return output
+
+def xor(data):
 	key = base64.b64encode(urandom(16)).decode('utf-8')
 	print(f"key: {key}")
 	print(f"len: {len(data)}\n")
 	l = len(key)
-	output_str = ""
+	output = ""
 
 	for i in range(len(data)):
 		current = data[i]
 		current_key = key[i % len(key)]
-		output_str += chr(current ^ ord(current_key))
+		output += chr(current ^ ord(current_key))
 	
-	output_str = '{ 0x' + ',0x'.join(hex(ord(x))[2:] for x in output_str) + ' };'
-	return output_str
+	output = '{ 0x' + ',0x'.join(hex(ord(x))[2:] for x in output) + ' };'
+	
+	return output
 
 def formatter(bin, format):
 	try:
 		data = open(bin, "rb").read()
 	except:
 		print('[!] Error while opening the file!')
-		sys.exit()
+		exit()
 
 	try:
 		binary_code = ''
 		for byte in data:
 			binary_code += "\\x" + hex(byte)[2:].zfill(2)
 	except:
-		print('[!] Error formatting the file!')
+		print('[!] Error formatting the bytes!')
+		exit()
 	
 	if format == "shellcode":
 		print("[+] Standard shellcode format:\n")
@@ -71,9 +91,16 @@ def formatter(bin, format):
 	elif format == "xor":
 		print("[+] Xor encrypt shellcode:\n")
 		print(xor(data))
+	elif format == "aes":
+		print("[+] AES encrypt shellcode:\n")
+		print(aesenc(data))
 	else:
 		print("[!] Output format not found!")
+		exit()
 
 if __name__ == '__main__':
-	args = parse_args()
-	formatter(args.bin, args.format)
+	try:
+		args = parse_args()
+		formatter(args.bin, args.format)
+	except KeyboardInterrupt:
+		print('[!] Stopping') 
