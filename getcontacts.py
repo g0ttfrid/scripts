b408c54eaa1cd8e36d19ddc955055a1660ae525c
g0ttfrid <yurimaia@protonmail.ch>
2024-01-23 02:01:44 -0300

    Update getcontacts.py

--- getcontacts.py
+++ getcontacts.py
@@ -6,12 +6,14 @@ from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter
 from unidecode import unidecode
 from fake_useragent import UserAgent
+from urllib.parse import unquote
 
 urllib3.disable_warnings()
 
 def parse_args():
     parser = ArgumentParser(usage='python3 getcontacts.py -t "Se7en Corp"', formatter_class=RawTextHelpFormatter)
     parser.add_argument('-t', '--target', type=str, required=True, help='insert company')
+    parser.add_argument('-p', '--proxy', type=str, help='insert proxy (e.g. 127.0.0.1:8080)')
     parser.add_argument('-f', '--format', type=str, help="\n"
     "linkedin (e.g. John Doe - Serial Killer - Se7en Corp)\n"
     "firstname.lastname (e.g. john.doe)\n"
@@ -32,92 +34,85 @@ def linked(target, proxy=None):
     cont = 0
 
     while True:
-        time.sleep(3.0)
-
         ua = UserAgent()
-        headers = {'User-Agent': f'{ua.random}', 'Referer': 'https://www.google.com.br'}
+        headers = {"Sec-Ch-Ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\"", "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Windows\"", "Upgrade-Insecure-Requests": "1", "User-Agent": f"{ua.random}", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", "X-Client-Data": "CNzeygE=", "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1", "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9", "Priority": "u=0, i", "Referer": "https://www.google.com.br"}
 
         try:
-            r = requests.get(f'https://www.google.com/search?q=site:linkedin.com/in+"{target}"&num=50&start={cont}', headers=headers, proxies=proxies, timeout=(5), verify=False)
+            r = requests.get(f'https://www.google.com.br/search?q=site:linkedin.com/in+"{target}"&num=50&start={cont}&cr=countryBR&tbs=ctr:countryBR,lr:lang_1pt&source=lnt&lr=lang_pt', headers=headers, proxies=proxies, timeout=(5), verify=False)
             r.raise_for_status()
         except requests.exceptions.RequestException as err:
-            print(f'\n[!] Error connection:\n{err}')
+            print(f'[!] Error connection:\n{err}')
             break
         
-        if 'encontrou nenhum documento correspondente.' in r.text:
+        if 'o encontrou nenhum document' in r.text:
             break
-        
-        try:
-            bs = BeautifulSoup(r.text, 'html.parser')
-            for link in bs.find_all('h3'):
-                if ' Linked' in link.text: data.add(link.text)
-        except AttributeError as err:
-            print(f'\n[!] Error scrap:\n{err}')
-            return None
 
+        bs = BeautifulSoup(r.text, 'html.parser')
+        for link in bs.find_all('a'):
+            if 'linkedin.com' in link.text: 
+                link_href = link.get('href')
+                h3_text = link.find('h3').text if link.find('h3') else "Nenhum título encontrado"
+                data.add(unquote(f"{h3_text} ({link_href})"))
+
+        time.sleep(5.0)
         cont += 50
     
     return data
 
+def generate_formats(data, format_type):
+    print(f"[+] {format_type}:")
+
+    for i in data:
+        full = unidecode((i.split(' - ')[0]).lower())
+        names = list(filter(lambda word: len(word) > 3 and word.isalnum(), full.split(' ')))
+        c = 1
+        while c < len(names):
+            if format_type == "firstname.lastname":
+                output.add(f"{names[0]}.{names[c]}")
+            elif format_type == "firstname_lastname":
+                output.add(f"{names[0]}_{names[c]}")
+            elif format_type == "firstnamelastname":
+                output.add(f"{names[0]}{names[c]}")
+            elif format_type == "firstletterlastname":
+                output.add(f"{names[0][0]}{names[c]}")
+            c += 1
+
+    return output
+
+def logger(target, list):
+    with open(f'{target}.txt', 'w', encoding='utf-8') as f:
+        for line in list:
+            f.write(f'{line}\n')
+
 if __name__ == '__main__':
     try:
         args = parse_args()
-        data = linked(args.target)
+        if args.proxy:
+            data = linked(args.target, args.proxy)
+        else:
+            data = linked(args.target)
+
+        output = set()
+
+        if data:
+            if args.format in ["firstname.lastname", "firstname_lastname", "firstnamelastname", "firstletterlastname"]:
+                output = generate_formats(data, args.format)
+            elif args.format == "linkedin" or args.format == None:
+                print("[+] LinkedIn:")
+                for i in data:
+                    line = i.strip()
+                    output.add(line)
+            else:
+                print("[!] Output format not found. \n[+] LinkedIn:")
+                for i in data:
+                    line = i.strip()
+                    output.add(line)
+                
+            logger(args.target, output)
+            print(*output, sep="\n")
+
+        else:
+            print(f'[!] Users not found\n')
+    
     except KeyboardInterrupt:
         print('\n[!] Stopping')
-        
-    output = set()
-    
-    if args.format == "linkedin" or args.format == None:
-        print("[+] LinkedIn:")
-        for i in data:
-            line = i.strip()
-            output.add(line)
-        print(*output, sep="\n")
-
-    elif args.format == "firstname.lastname":
-        print("\n[+] Firstname.Lastname (e.g. john.doe):")
-        for i in data:
-            full = unidecode((i.split(' - ')[0]).lower())
-            names = list(filter(lambda word: len(word)>3 and word.isalnum(),full.split(' ')))
-            c = 1
-            while c < len(names):
-                output.add(f"{names[0]}.{names[c]}")
-                c += 1
-        print(*output, sep="\n")
-
-    elif args.format == "firstname_lastname":
-        print("\n[+] Firstname_Lastname (e.g. john_doe):")
-        for i in data:
-            full = unidecode((i.split(' - ')[0]).lower())
-            names = list(filter(lambda word: len(word)>3 and word.isalnum(),full.split(' ')))
-            c = 1
-            while c < len(names):
-                output.add(f"{names[0]}_{names[c]}")
-                c += 1
-        print(*output, sep="\n")
-
-    elif args.format == "firstnamelastname":
-        print("\n[+] FirstnameLastname (e.g. johndoe):")
-        for i in data:
-            full = unidecode((i.split(' - ')[0]).lower())
-            names = list(filter(lambda word: len(word)>3 and word.isalnum(),full.split(' ')))
-            c = 1
-            while c < len(names):
-                output.add(f"{names[0]}{names[c]}")
-                c += 1
-        print(*output, sep="\n")
-
-    elif args.format == "firstletterlastname":
-        print("\n[+] FirstletterLastname (e.g. jdoe):")
-        for i in data:
-            full = unidecode((i.split(' - ')[0]).lower())
-            names = list(filter(lambda word: len(word)>3 and word.isalnum(),full.split(' ')))
-            c = 1
-            while c < len(names):
-                output.add(f"{names[0][0]}{names[c]}")
-                c += 1
-        print(*output, sep="\n")
-
-    else:
-        print("[!] Output format not found!")
